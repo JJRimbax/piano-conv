75d41d1d33e2ceeeb84c1ba39a9e67ab7e12cb24
Jules Raimbaud <jules.raimbaud@gmail.com>
2024-09-20 14:17:06 +0200

    add trash and style

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet, TextInput, ScrollView, Pressable } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+  Dimensions,
+} from 'react-native';
 import { Audio } from 'expo-av';
 import { Svg, Rect } from 'react-native-svg';
-
-// Importation des fichiers audio
+import { MaterialIcons } from '@expo/vector-icons';
 import C_mp3 from './assets/notes/C.mp3';
 import Csharp_mp3 from './assets/notes/Csharp.mp3';
 import D_mp3 from './assets/notes/D.mp3';
@@ -32,13 +39,14 @@ import C3_mp3 from './assets/notes/C3.mp3';
 import Csharp3_mp3 from './assets/notes/Csharp3.mp3';
 import D3_mp3 from './assets/notes/D3.mp3';
 
-// Dimensions des touches
-const WHITE_KEY_WIDTH = 50;
-const WHITE_KEY_HEIGHT = 200;
-const BLACK_KEY_WIDTH = 30;
-const BLACK_KEY_HEIGHT = 120;
 
-// Données combinées des touches avec lettres de A à Z
+const screenWidth = Dimensions.get('window').width;
+
+
+const TOTAL_WIDTH = screenWidth * 0.9;
+const INPUT_WIDTH = TOTAL_WIDTH * 0.8; 
+const BUTTON_WIDTH = TOTAL_WIDTH * 0.2;
+
 const keysData = [
   { letter: 'A', note: 'C', file: C_mp3, type: 'white', index: 0 },
   { letter: 'B', note: 'C#', file: Csharp_mp3, type: 'black', whiteIndex: 0 },
@@ -66,13 +74,19 @@ const keysData = [
   { letter: 'X', note: 'B2', file: B2_mp3, type: 'white', index: 13 },
   { letter: 'Y', note: 'C3', file: C3_mp3, type: 'white', index: 14 },
   { letter: 'Z', note: 'C#3', file: Csharp3_mp3, type: 'black', whiteIndex: 14 },
-  { letter: ' ', note: 'D3', file: D3_mp3, type: 'white', index: 15 }, // Espace pour la dernière touche
+  { letter: ' ', note: 'D3', file: D3_mp3, type: 'white', index: 15 }, 
 ];
+
+const numWhiteKeys = keysData.filter((key) => key.type === 'white').length;
+
+const WHITE_KEY_WIDTH = TOTAL_WIDTH / numWhiteKeys;
+const WHITE_KEY_HEIGHT = 200;
+const BLACK_KEY_WIDTH = WHITE_KEY_WIDTH * 0.6; 
+const BLACK_KEY_HEIGHT = WHITE_KEY_HEIGHT * 0.6;
 
 export default function App() {
   const [text, setText] = useState('');
 
-  // Fonction pour jouer une note
   const playNote = async (file, letter) => {
     if (file) {
       const { sound } = await Audio.Sound.createAsync(file);
@@ -87,93 +101,120 @@ export default function App() {
     }
   };
 
+  const clearText = () => {
+    setText('');
+  };
+
   return (
-    <ScrollView
-      horizontal={true}
-      contentContainerStyle={styles.scrollContainer}
-      scrollEnabled={true}
-    >
-      <View style={styles.container}>
+    <View style={styles.appContainer}>
+      <View style={styles.textInputContainer}>
         <TextInput
           style={styles.input}
           value={text}
           placeholder="Joue les notes ici"
           editable={false}
         />
-        <View style={styles.pianoContainer}>
-          {/* Rendu des touches blanches */}
-          {keysData
-            .filter((key) => key.type === 'white')
-            .map((key) => (
-              <Pressable
-                key={`white-${key.index}`}
-                style={[
-                  styles.whiteKey,
-                  { left: key.index * WHITE_KEY_WIDTH },
-                ]}
-                onPress={() => playNote(key.file, key.letter)}
-              >
-                <Svg height={WHITE_KEY_HEIGHT} width={WHITE_KEY_WIDTH}>
-                  <Rect
-                    x="0"
-                    y="0"
-                    width={WHITE_KEY_WIDTH}
-                    height={WHITE_KEY_HEIGHT}
-                    fill="white"
-                    stroke="black"
-                    strokeWidth="2"
-                  />
-                </Svg>
-                <Text style={styles.letter}>{key.letter}</Text>
-              </Pressable>
-            ))}
-
-          {/* Rendu des touches noires */}
-          {keysData
-            .filter((key) => key.type === 'black')
-            .map((key) => (
-              <Pressable
-                key={`black-${key.whiteIndex}`}
-                style={[
-                  styles.blackKey,
-                  {
-                    left:
-                      (key.whiteIndex + 1) * WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2,
-                  },
-                ]}
-                onPress={() => playNote(key.file, key.letter)}
-              >
-                <Svg height={BLACK_KEY_HEIGHT} width={BLACK_KEY_WIDTH}>
-                  <Rect
-                    x="0"
-                    y="0"
-                    width={BLACK_KEY_WIDTH}
-                    height={BLACK_KEY_HEIGHT}
-                    fill="black"
-                  />
-                </Svg>
-                <Text style={styles.blackLetter}>{key.letter}</Text>
-              </Pressable>
-            ))}
-        </View>
+        <TouchableOpacity onPress={clearText} style={styles.clearButton}>
+          <MaterialIcons name="delete" size={24} color="white" />
+        </TouchableOpacity>
       </View>
-    </ScrollView>
+      <View style={styles.pianoContainer}>
+        {keysData
+          .filter((key) => key.type === 'white')
+          .map((key) => (
+            <Pressable
+              key={`white-${key.index}`}
+              style={[
+                styles.whiteKey,
+                { left: key.index * WHITE_KEY_WIDTH },
+              ]}
+              onPress={() => playNote(key.file, key.letter)}
+            >
+              <Svg height={WHITE_KEY_HEIGHT} width={WHITE_KEY_WIDTH}>
+                <Rect
+                  x="0"
+                  y="0"
+                  width={WHITE_KEY_WIDTH}
+                  height={WHITE_KEY_HEIGHT}
+                  fill="white"
+                  stroke="black"
+                  strokeWidth="2"
+                />
+              </Svg>
+              <Text style={styles.letter}>{key.letter}</Text>
+            </Pressable>
+          ))}
+        {keysData
+          .filter((key) => key.type === 'black')
+          .map((key) => (
+            <Pressable
+              key={`black-${key.whiteIndex}`}
+              style={[
+                styles.blackKey,
+                {
+                  left:
+                    (key.whiteIndex + 1) * WHITE_KEY_WIDTH - BLACK_KEY_WIDTH / 2,
+                },
+              ]}
+              onPress={() => playNote(key.file, key.letter)}
+            >
+              <Svg height={BLACK_KEY_HEIGHT} width={BLACK_KEY_WIDTH}>
+                <Rect
+                  x="0"
+                  y="0"
+                  width={BLACK_KEY_WIDTH}
+                  height={BLACK_KEY_HEIGHT}
+                  fill="black"
+                />
+              </Svg>
+              <Text style={styles.blackLetter}>{key.letter}</Text>
+            </Pressable>
+          ))}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  container: {
+  appContainer: {
+    flex: 1,
     backgroundColor: '#fff',
+    justifyContent: 'flex-end', 
+    alignItems: 'center',
+    paddingBottom: 20, 
+  },
+  textInputContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 20,
+    width: TOTAL_WIDTH,
+    justifyContent: 'center',
+  },
+  input: {
+    height: 50,
+    width: INPUT_WIDTH,
+    borderColor: 'gray',
+    borderWidth: 1,
+    borderRadius: 25,
+    paddingHorizontal: 20,
+    fontSize: 18,
+    backgroundColor: '#f2f2f2',
+    textAlign: 'center',
+  },
+  clearButton: {
+    marginLeft: 10,
+    backgroundColor: '#ff4d4d',
     padding: 10,
+    borderRadius: 25,
+    width: BUTTON_WIDTH - 20, 
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   pianoContainer: {
     position: 'relative',
-    width: WHITE_KEY_WIDTH * keysData.filter((key) => key.type === 'white').length,
+    width: TOTAL_WIDTH,
     height: WHITE_KEY_HEIGHT,
+    alignSelf: 'center',
   },
   whiteKey: {
     position: 'absolute',
@@ -187,14 +228,6 @@ const styles = StyleSheet.create({
     height: BLACK_KEY_HEIGHT,
     alignItems: 'center',
     zIndex: 1,
-  },
-  input: {
-    height: 40,
-    width: '80%',
-    borderColor: 'gray',
-    borderWidth: 1,
-    marginBottom: 20,
-    textAlign: 'center',
   },
   letter: {
     position: 'absolute',
